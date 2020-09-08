f6b56b0d8c2e4fcd1f2bda88144116b46e6e5fbc
eranCat <eranka12@gmail.com>
2020-09-08 16:03:56 +0300

    removed fields

--- logic.js
+++ logic.js
@@ -26,23 +26,11 @@ function createDataAsText() {
 
 function createData(returnedFormat) {
     return new DataClass(
-        1, "delivery", 35, 5,
-        "Some buisness name",
-        10, null,
-        2, null,
-        "Eran", null,
-        "holon", null,
-        "histadrut",
-        4, "a", 1, 22,
-        "052-222222", "053-333333",
-        "asmahta", 33, "", "",
+        3, "delivery", 35, 5,10,"Eran", 
+        "holon", "hahistadrut",4,
+        "052-222222",
         "sec asmahta",
-        "20/09/2020", "13:13",
-        99, 100, "10/10/2020",
-        "", 235245, 456456,
-        returnedFormat, null, "",
-        Date.now(),
-        "eee@eee.eee",
-        "20/11/2020", "20:20"
+        returnedFormat,
+        "eee@eee.eee"
     )
 }
